2008e586dacb2569c3e24f24967b799d5010ddd1
Sebastian Krzyszkowiak <dos@dosowisko.net>
2019-12-04 20:37:47 +0100

    anim: fix skipping repeated animations

--- src/gamestates/anim.c
+++ src/gamestates/anim.c
@@ -191,6 +191,7 @@ void Gamestate_ProcessEvent(struct Game* game, struct GamestateResources* data, 
 		data->finished = true;
 		data->frozen = false;
 		data->freezeno++;
+		data->repeats = 0;
 	}
 	if (game->_priv.showconsole && ((ev->type == ALLEGRO_EVENT_KEY_DOWN) && (ev->keyboard.keycode == ALLEGRO_KEY_COMMA))) {
 		data->delay = 0.01;
@@ -201,6 +202,7 @@ void Gamestate_ProcessEvent(struct Game* game, struct GamestateResources* data, 
 		}
 		data->frozen = false;
 		data->freezeno++;
+		data->repeats = 0;
 	}
 }
 
